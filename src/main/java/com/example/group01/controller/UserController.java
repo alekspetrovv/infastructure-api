043151960ed00392f6cffec259771d426f0121f2
Petrov,Aleks A.P <aleks.petrov@student.fontys.nl>
2021-04-15 19:02:37 +0200

    "user validation"

--- src/main/java/com/example/group01/controller/UserController.java
+++ src/main/java/com/example/group01/controller/UserController.java
@@ -5,12 +5,15 @@ import com.example.group01.modules.User;
 import com.example.group01.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 @CrossOrigin("/user")
 public class UserController {
 
@@ -33,16 +36,39 @@ public class UserController {
         return new ResponseEntity<>(getUser, HttpStatus.OK);
     }
 
-    @PostMapping("/add")
-    public ResponseEntity<User> create(@RequestBody User user) {
+    @PostMapping(value = "")
+    public ResponseEntity<User> create(
+            @NotBlank @RequestParam("email") String email,
+            @NotBlank @RequestParam("password") String password,
+            @NotBlank @RequestParam("firstName") String firstName,
+            @NotBlank @RequestParam("lastName") String lastName
+    ) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
         User newUser = userService.create(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
 
-    @PutMapping("/update")
-    public ResponseEntity<User> update(@RequestBody User user) {
-        User updateUser = userService.update(user);
-        return new ResponseEntity<>(updateUser, HttpStatus.CREATED);
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(
+            @NotBlank @RequestParam("email") String email,
+            @NotBlank @RequestParam("password") String password,
+            @NotBlank @RequestParam("firstName") String firstName,
+            @NotBlank @RequestParam("lastName") String lastName,
+            @NotBlank @PathVariable long id
+    ) {
+        User user = userService.findUserById(id);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+
+        User updatedUser = userService.update(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.CREATED);
     }
 
 
